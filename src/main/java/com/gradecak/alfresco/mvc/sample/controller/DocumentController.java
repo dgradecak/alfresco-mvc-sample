17da43dfe907397d9724505f893c0a505e88ad26
Daniel Gradecak <daniel@gradecak.com>
2016-01-18 11:57:42 +0100

    updated to alfresco @mvc 4.0.0-RELEASE

--- src/main/java/com/gradecak/alfresco/mvc/sample/controller/DocumentController.java
+++ src/main/java/com/gradecak/alfresco/mvc/sample/controller/DocumentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.gradecak.alfresco.mvc.JsonUtils;
+import com.gradecak.alfresco.mvc.ResponseMapBuilder;
 import com.gradecak.alfresco.mvc.sample.domain.Document;
 import com.gradecak.alfresco.mvc.sample.service.DocumentService;
 
@@ -23,29 +23,30 @@ import com.gradecak.alfresco.mvc.sample.service.DocumentService;
 @RequestMapping("/document/**")
 public class DocumentController {
 
-	@Autowired
-	private DocumentService documentService;
+  @Autowired
+  private DocumentService documentService;
 
-	@RequestMapping(value = "sample", method = { RequestMethod.POST, RequestMethod.GET })
-	@ResponseBody
-	public Map<String, Object> sample(@RequestBody final Document docCreated) throws IOException {
-		
-		// for this show case purposes we call create which is marked as AlfrescoTransaction
-		NodeRef createdRef = documentService.create(docCreated);
+  @RequestMapping(value = "sample", method = { RequestMethod.POST, RequestMethod.GET })
+  @ResponseBody
+  public Map<String, Object> sample(@RequestBody final Document docCreated) throws IOException {
 
-		// in a second transaction we do the update
-		Document document = documentService.get(createdRef);
-		document.setDescription(document.getDescription() + " updated!");
-		documentService.update(createdRef, document);
+    // for this show case purposes we call create which is marked as
+    // AlfrescoTransaction
+    NodeRef createdRef = documentService.create(docCreated);
 
-		// ... yet another transaction
-		return JsonUtils.createResponseMap(documentService.get(createdRef), true);
-	}
-	
-	@RequestMapping(value = "findAll", method = { RequestMethod.POST, RequestMethod.GET })
-	@ResponseBody
-	public Map<String, Object> findAll() throws IOException {
-		return JsonUtils.createResponseMap(documentService.findAll(), true);
-	}
+    // in a second transaction we do the update
+    Document document = documentService.get(createdRef);
+    document.setDescription(document.getDescription() + " updated!");
+    documentService.update(createdRef, document);
+
+    // ... yet another transaction
+    return ResponseMapBuilder.createResponseMap(documentService.get(createdRef), true).build();
+  }
+
+  @RequestMapping(value = "findAll", method = { RequestMethod.POST, RequestMethod.GET })
+  @ResponseBody
+  public Map<String, Object> findAll() throws IOException {
+    return ResponseMapBuilder.createResponseMap(documentService.findAll(), true).build();
+  }
 
 }
--- src/main/java/com/gradecak/alfresco/mvc/sample/mapper/DocumentNodeMapper.java
+++ src/main/java/com/gradecak/alfresco/mvc/sample/mapper/DocumentNodeMapper.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
 
 import com.gradecak.alfresco.mvc.mapper.NodePropertiesMapper;
@@ -12,17 +13,17 @@ import com.gradecak.alfresco.mvc.sample.domain.Document;
 
 public class DocumentNodeMapper implements NodePropertiesMapper<Document> {
 
-	public Document mapNodeProperties(Map<QName, Serializable> properties) {
-		Document dl = new Document();
-		dl.setDescription((String) properties.get(ContentModel.PROP_DESCRIPTION));
+  public Document mapNodeProperties(NodeRef nodeRef, Map<QName, Serializable> properties) {
+    Document dl = new Document();
+    dl.setDescription((String) properties.get(ContentModel.PROP_DESCRIPTION));
 
-		return dl;
-	}
+    return dl;
+  }
 
-	// TODO not sure for now if this should be a simple helper
-	public static Map<QName, Serializable> mapBean(Document bean) {
-		Map<QName, Serializable> map = new HashMap<QName, Serializable>();
-		map.put(ContentModel.PROP_DESCRIPTION, bean.getDescription());
-		return map;
-	}
+  // TODO this is a simple helper
+  public static Map<QName, Serializable> mapBean(Document bean) {
+    Map<QName, Serializable> map = new HashMap<QName, Serializable>();
+    map.put(ContentModel.PROP_DESCRIPTION, bean.getDescription());
+    return map;
+  }
 }
