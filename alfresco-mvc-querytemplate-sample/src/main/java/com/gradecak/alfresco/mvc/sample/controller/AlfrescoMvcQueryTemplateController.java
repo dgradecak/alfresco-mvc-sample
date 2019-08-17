35cc20f3e3e9ac34d664d73f0da5eb88703ae6fe
Daniel Gradecak <daniel@pleosoft.com>
2019-08-17 22:04:06 +0200

    https://github.com/dgradecak/alfresco-mvc/issues/17

--- alfresco-mvc-querytemplate-sample/src/main/java/com/gradecak/alfresco/mvc/sample/controller/AlfrescoMvcQueryTemplateController.java
+++ alfresco-mvc-querytemplate-sample/src/main/java/com/gradecak/alfresco/mvc/sample/controller/AlfrescoMvcQueryTemplateController.java
@@ -6,14 +6,13 @@ import org.alfresco.rest.framework.resource.parameters.CollectionWithPagingInfo;
 import org.alfresco.rest.framework.resource.parameters.Paging;
 import org.alfresco.rest.framework.resource.parameters.Params;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.gradecak.alfresco.mvc.annotation.AlfrescoTransaction;
-import com.gradecak.alfresco.mvc.rest.AlfrescoResponseEntity;
+import com.gradecak.alfresco.mvc.rest.annotation.AlfrescoRestResponse;
 import com.gradecak.alfresco.mvc.sample.domain.CmFolder;
 import com.gradecak.alfresco.mvc.sample.service.QueryTemplateService;
 
@@ -23,6 +22,7 @@ import com.gradecak.alfresco.mvc.sample.service.QueryTemplateService;
 
 @Controller
 @RequestMapping("/querytemplate")
+@AlfrescoRestResponse
 public class AlfrescoMvcQueryTemplateController {
 
   @Autowired
@@ -31,25 +31,25 @@ public class AlfrescoMvcQueryTemplateController {
   @AlfrescoTransaction(readOnly = true)
   @RequestMapping(value = "sample", method = { RequestMethod.GET })
   public ResponseEntity<?> sample() throws IOException {
-    return new ResponseEntity<>(service.getCompanyHomeFolder(), HttpStatus.OK);
+    return ResponseEntity.ok(service.getCompanyHomeFolder());
   }
 
   @AlfrescoTransaction(readOnly = true)
   @RequestMapping(value = "search", method = { RequestMethod.GET })
   public ResponseEntity<?> search() {
     CollectionWithPagingInfo<CmFolder> collection = CollectionWithPagingInfo.asPaged(Paging.DEFAULT, service.search());
-    return new ResponseEntity<>(collection, HttpStatus.OK);
+    return ResponseEntity.ok(collection);
   }
 
   @AlfrescoTransaction(readOnly = true)
   @RequestMapping(value = "nodes", method = { RequestMethod.GET })
   public ResponseEntity<?> nodes(Params params) throws IOException {
-    return new AlfrescoResponseEntity<>(service.rootNodes(params), HttpStatus.OK);
+    return ResponseEntity.ok(service.rootNodes(params));
   }
 
   @AlfrescoTransaction(readOnly = true)
   @RequestMapping(value = "node", method = { RequestMethod.GET })
   public ResponseEntity<?> node() throws IOException {
-    return new AlfrescoResponseEntity<>(service.findRootNode(), HttpStatus.OK);
+    return ResponseEntity.ok(service.findRootNode());
   }
 }
