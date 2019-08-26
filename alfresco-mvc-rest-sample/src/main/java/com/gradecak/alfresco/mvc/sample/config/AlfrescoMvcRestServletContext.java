9bf006bfcfddaca89ada62f13ef1ead4ebdaf687
Daniel Gradecak <daniel@pleosoft.com>
2019-08-26 11:29:25 +0200

    renamed annotation

--- alfresco-mvc-rest-sample/src/main/java/com/gradecak/alfresco/mvc/sample/config/AlfrescoMvcRestServletContext.java
+++ alfresco-mvc-rest-sample/src/main/java/com/gradecak/alfresco/mvc/sample/config/AlfrescoMvcRestServletContext.java
@@ -7,12 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.gradecak.alfresco.mvc.rest.annotation.EnableAlfrescoMvcWeb;
+import com.gradecak.alfresco.mvc.rest.annotation.EnableWebAlfrescoMvc;
 import com.gradecak.alfresco.mvc.sample.controller.AlfrescoMvcRestController;
 import com.gradecak.alfresco.mvc.sample.service.SampleService;
 
 @Configuration
-@EnableAlfrescoMvcWeb
+@EnableWebAlfrescoMvc
 @ComponentScan(basePackageClasses = { AlfrescoMvcRestController.class })
 public class AlfrescoMvcRestServletContext implements WebMvcConfigurer {
 
