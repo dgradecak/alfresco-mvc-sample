ff7eb3d2a2663e96daa6a6a87f08c05d5fb5e667
Daniel Gradecak <daniel@pleosoft.com>
2021-03-16 15:50:04 +0100

    references #20

--- alfresco-mvc-rest-sample/src/main/java/com/gradecak/alfresco/mvc/sample/config/AlfrescoMvcRestModuleConfig.java
+++ alfresco-mvc-rest-sample/src/main/java/com/gradecak/alfresco/mvc/sample/config/AlfrescoMvcRestModuleConfig.java
@@ -1,11 +1,8 @@
 package com.gradecak.alfresco.mvc.sample.config;
 
-import org.springframework.context.annotation.Configuration;
-
 import com.gradecak.alfresco.mvc.rest.annotation.AlfrescoDispatcherWebscript;
 import com.gradecak.alfresco.mvc.rest.annotation.EnableAlfrescoMvcRest;
 
-@Configuration
 @EnableAlfrescoMvcRest(@AlfrescoDispatcherWebscript(servletContext = AlfrescoMvcRestServletContext.class))
 public class AlfrescoMvcRestModuleConfig {
 }
