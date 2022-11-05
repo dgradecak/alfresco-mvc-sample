30d1c78c92aad8a8cb3460c3af1f1e58fd2d65dc
Daniel Gradecak <daniel@gradecak.com>
2022-11-05 18:41:57 +0100

    using alfresco-mvc-bom

--- alfresco-mvc-rest-sample/src/main/java/com/gradecak/alfresco/mvc/sample/controller/AlfrescoMvcRestController.java
+++ alfresco-mvc-rest-sample/src/main/java/com/gradecak/alfresco/mvc/sample/controller/AlfrescoMvcRestController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +57,11 @@ public class AlfrescoMvcRestController {
 
 	@GetMapping(value = "noderef")
 	public ResponseEntity<?> noderef(@RequestParam NodeRef nodeRef) throws IOException {
+		return ResponseEntity.ok(nodeRef);
+	}
+
+	@GetMapping(value = "noderef/{nodeRef}")
+	public ResponseEntity<?> noderefPath(@PathVariable NodeRef nodeRef) throws IOException {
 		return ResponseEntity.ok(nodeRef);
 	}
 
